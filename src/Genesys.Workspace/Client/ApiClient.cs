3506fd13ca7dc548374503e560c534a421926b66
Jim Crespino <jim.crespino@genesys.copm>
2017-10-26 16:12:26 -0500

    Update project file

--- src/Genesys.Workspace/Client/ApiClient.cs
+++ src/Genesys.Workspace/Client/ApiClient.cs
@@ -146,6 +146,11 @@ namespace Genesys.Workspace.Client
                     request.AddParameter(contentType, postBody, ParameterType.RequestBody);
                 }
             }
+            else
+            {
+                // empty body, but GWS expects content type to be set.
+                request.AddHeader("Content-type", contentType);
+            }
 
             return request;
         }
